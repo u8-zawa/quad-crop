cc4cf163f67b554fbcbb168193cbe8528902d266
u8-zawa <yuya0713.a@gmail.com>
2025-08-17 14:00:08 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    build: {
-        target: 'esnext',
-        sourcemap: false,
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                drop_console: true,
-                drop_debugger: true,
+export default defineConfig(({ command }) => {
+    return {
+        base: command === 'build' ? '/quad-crop/' : './',
+        build: {
+            target: 'esnext',
+            sourcemap: false,
+            minify: 'terser',
+            terserOptions: {
+                compress: {
+                    drop_console: true,
+                    drop_debugger: true,
+                },
             },
         },
-    },
-    server: {
-        open: true
-        // https: true,
+        server: {
+            // https: true,
+        }
     }
 });
